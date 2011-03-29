eb5e2542836af6058c533dfb7f4b1209ccbf2777
JoungKyun Kim <joungkyun@daum.net>
2011-03-29 18:24:11 +0000

    changed ansi constants value

--- libidn.h
+++ libidn.h
@@ -5,33 +5,15 @@
  * This file includes proto type of idn apis
  *
  * @author	JoungKyun.Kim <http://oops.org>
- * $Date: 2011-03-01 04:17:47 $
- * $Revision: 1.9 $
+ * $Date: 2011-03-29 18:24:11 $
+ * $Revision: 1.9.2.1 $
  * @attention	Copyright (c) 2011 JoungKyun.Kim all rights reserved.
  */
-/* $Id: libidn.h,v 1.9 2011-03-01 04:17:47 oops Exp $ */
+/* $Id: libidn.h,v 1.9.2.1 2011-03-29 18:24:11 oops Exp $ */
 #ifndef LIBIDN_H
 #define LIBIDN_H
 
 #include <olibc/oc_type.h>
-
-#define ENDANSI		0
-#define GRAY		1
-#define BGRAY		2
-#define RED			3
-#define BRED		4
-#define GREEN		5
-#define BGREEN		6
-#define YELLOW		7
-#define BYELLOW		8
-#define BLUE		9
-#define BBLUE		10
-#define MAGENTA		11
-#define BMAGENTA	12
-#define CYAN		13
-#define BCYAN		14
-#define WHITE		15
-#define BWHITE		16
 
 extern const char * stringprep_locale_charset_cache;
 UInt convert_punycode_r (CChar * src, UChar ** dst, bool mode, CChar * charset);
