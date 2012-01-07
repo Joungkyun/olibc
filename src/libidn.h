e3e10d15737f2600c94be9ef37c768d4d47dffa6
JoungKyun Kim <joungkyun@daum.net>
2012-01-07 17:33:01 +0000

    removed unused constants

--- src/libidn.h
+++ src/libidn.h
@@ -39,24 +39,6 @@ extern "C"
 {
 # endif
 
-#define ENDANSI		0
-#define GRAY		1
-#define BGRAY		2
-#define RED			3
-#define BRED		4
-#define GREEN		5
-#define BGREEN		6
-#define YELLOW		7
-#define BYELLOW		8
-#define BLUE		9
-#define BBLUE		10
-#define MAGENTA		11
-#define BMAGENTA	12
-#define CYAN		13
-#define BCYAN		14
-#define WHITE		15
-#define BWHITE		16
-
 extern CChar * stringprep_locale_charset_cache;
 extern char * convert_punycode (CChar * src, CChar * charset);
 
